8494a3e2aeb37f2e1639b3bac06561f53000ee31
RAVEL SIMBA <156199207+simbaravel23@users.noreply.github.com>
2024-01-26 19:46:39 -0300

    variaveis

---
+++ script.js
@@ -0,0 +1,11 @@
+var nome = 'Andre';
+var idade = 28;
+var possuiFaculdade = true;
+
+console.log(nome, idade, possuiFaculdade);
+
+const preco= 25;
+const totalComprado = 5;
+const totalPreco = totalComprado * preco;
+
+console.log(totalPreco);
