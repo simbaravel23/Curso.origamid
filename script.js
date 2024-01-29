8bc7ddbdb88f6f0b0b6b555f91db1b25d749073b
RAVEL SIMBA <156199207+simbaravel23@users.noreply.github.com>
2024-01-29 18:02:39 -0300

    n

--- script.js
+++ script.js
@@ -1,8 +1,21 @@
-var nome = 'Andre';
-var idade = 28;
+var nome = 'Ravel';
+var nome = 'Lucas';
+
+let idade = 37;
+//let idade = 38: //não funciona assim, dá erro!
+idade = 38
+
+var comidaFavorita = 'Rondele';
+//var comidaFavorita = 'musse de maracuja'
+
+
+var time = 'sao paulo'
+ professor = 'ravel';
+  
+
 var possuiFaculdade = true;
 
-console.log(nome, idade, possuiFaculdade);
+console.log(nome, idade, possuiFaculdade, comidaFavorita, time);
 
 const preco= 25;
 const totalComprado = 5;
---
+++ tipos de dados/script.js
@@ -0,0 +1,6 @@
+var nome = 'Ravel'
+let ano = "2024 "
+const idade = 37
+var sobrenome = 'Silva ' 
+var nomeCompleto = `${nome} ${sobrenome}`
+console.log(nomeCompleto, ano, idade )
